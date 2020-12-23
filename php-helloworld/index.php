519c3f1c1c81fd4775866f31ba10ed4ada045a53
Student User <student@workstation.lab.example.com>
2020-12-23 08:06:41 -0500

    test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, seif World! php version is " . PHP_VERSION . "\n";
+print "Hello, seijjdecjnnf World! php version is " . PHP_VERSION . "\n";
 print " A change is a a coming!\n";
 ?>
